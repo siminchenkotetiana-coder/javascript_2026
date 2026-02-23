cc123391924c51340b3867b13a160ad9f16e2911
siminchenkotetiana-coder <siminchenko.tetiana@krfk.kai.edu.ua>
2026-02-23 09:54:42 +0200

    Update script1.js

--- js/lab2/script1.js
+++ js/lab2/script1.js
@@ -1,9 +1,34 @@
-// Створіть три змінні, використовуючи var, let, і const. Присвойте їм відповідні значення.
+// 1. Створіть три змінні, використовуючи var, let, і const. Присвойте їм відповідні значення.
 
 var numberVar = 5; 
-let numberLet = 45;
-const numberConst = 30;
+let numberLet = "45";
+const numberConst = null;
 
-// Визначте тип кожної змінної за допомогою оператора typeof і виведіть результат у консоль.
+// 2. Визначте тип кожної змінної за допомогою оператора typeof і виведіть результат у консоль.
 
+console.log(typeof numberVar);
+console.log(typeof numberLet);
+console.log(typeof numberConst);
 
+// 3. Створіть об'єкт із полями: ім'я, вік, статус студента (булевий тип).
+
+let student = {
+    name: "Mark",
+    age: "20",
+    isTrue: false
+};
+
+// 4. Додайте до об'єкта нове поле "університет" і виведіть об'єкт у консоль.
+
+student.university = "KRFK KAI"
+console.log(student)
+
+// 5. Виконайте зміну типу змінної, що була оголошена за допомогою let. Наприклад, змініть її значення з числа на рядок.
+
+numberLet = "Тепер це рядок";
+console.log(numberLet);
+console.log(typeof numberLet);
+
+// 6. Спробуйте змінити значення змінної, оголошеної через const. Перевірте, що станеться.
+
+numberConst = 5; 
