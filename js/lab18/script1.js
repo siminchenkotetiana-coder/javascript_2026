5095d7921fe5fa550bb94f05a755e0cbf5d440e7
siminchenkotetiana-coder <siminchenko.tetiana@krfk.kai.edu.ua>
2026-05-26 10:48:53 +0300

    Update script1.js

--- js/lab18/script1.js
+++ js/lab18/script1.js
@@ -9,3 +9,21 @@ const filteredWords = words.filter((word) =>
 
 console.log("Початковий масив слів:", words);
 console.log(`Слова, що починаються на літеру '${firstLetter}':`, filteredWords);
+
+
+// Додаткове завдання: вилучити з масиву всі числа, що знаходяться в заданому діапазоні.
+
+const numbers = [7, 12, 18, 25, 30, 41, 50, 77];
+
+// Межі діапазону
+const min = 5;
+const max = 30;
+
+const filteredNumbers = numbers.filter(
+  (num) => num < min || num > max
+);
+
+console.log("Початковий масив:", numbers);
+console.log(`Масив без чисел у діапазоні від ${min} до ${max}:`, filteredNumbers);
+
+
