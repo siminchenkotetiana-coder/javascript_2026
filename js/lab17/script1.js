350ac48a328e33892c8623905ccb9d15f9edc44c
siminchenkotetiana-coder <siminchenko.tetiana@krfk.kai.edu.ua>
2026-05-26 10:38:53 +0300

    Update script1.js

--- js/lab17/script1.js
+++ js/lab17/script1.js
@@ -5,3 +5,20 @@ const roundedNumbers = numbers.map((num) => Math.round(num));
 
 console.log("Початковий масив:", numbers);
 console.log("Округлений масив:", roundedNumbers);
+
+
+// Додаткове завдання:
+// Перетворити масив об'єктів з координатами у формат "x, y".
+
+const points = [
+    { x: 2, y: 5 },
+    { x: -3, y: 7 },
+    { x: 10, y: 0 },
+    { x: 4, y: -8 }
+];
+
+const formattedPoints = points.map((point) => {
+    return `${point.x}, ${point.y}`;
+});
+
+console.log("Координати у форматі 'x, y':", formattedPoints);
