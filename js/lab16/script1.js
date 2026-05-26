1d9b9538bd5883dc75324f978cbe14e1eb3b54d6
siminchenkotetiana-coder <siminchenko.tetiana@krfk.kai.edu.ua>
2026-05-26 10:33:36 +0300

    Update script1.js

--- js/lab16/script1.js
+++ js/lab16/script1.js
@@ -12,3 +12,24 @@ numbers.forEach((num) => {
 
 console.log("Масив:", numbers);
 console.log(`Кількість елементів, що задовольняють умову: ${count}`);
+
+// Додаткове завдання:
+// Додати до кожного елемента масиву певний суфікс.
+
+// Додаткове завдання:
+// Додати до кожного елемента масиву певний суфікс.
+
+const fruits = ["apple", "banana", "orange", "grape", "melon"];
+
+const suffix = "_fruit";
+
+const updatedFruits = fruits.map((fruit) => {
+    return fruit + suffix;
+});
+
+console.log("Початковий масив:", fruits);
+console.log("Масив після додавання суфікса:", updatedFruits);
+
+
+
+
