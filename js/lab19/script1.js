79a2a30f148420a4c5fbc6bcf2459106a7959d87
siminchenkotetiana-coder <siminchenko.tetiana@krfk.kai.edu.ua>
2026-05-27 08:30:55 +0300

    Update script1.js

--- js/lab19/script1.js
+++ js/lab19/script1.js
@@ -18,3 +18,18 @@ const dictionary = words.reduce((accumulator, item) => {
 
 console.log("Початковий масив:", words);
 console.log("Словник:", dictionary);
+
+
+// Додаткове завдання:
+// Перетворити вкладений масив у плоский.
+
+const nestedArray = [1, 2, [3, 4], [5, 6], 7, [8, 9]];
+
+// Перетворення вкладеного масиву у плоский
+const flatArray = nestedArray.flat();
+
+console.log("Вкладений масив:", nestedArray);
+
+console.log("Плоский масив:", flatArray);
+
+
