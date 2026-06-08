f11f28d0b88f0a1adbf23b30b3316a040eaf8ccb
siminchenkotetiana-coder <siminchenko.tetiana@krfk.kai.edu.ua>
2026-06-08 09:07:19 +0300

    Update script.js

--- js/lab30/script.js
+++ js/lab30/script.js
@@ -2,8 +2,15 @@ function showDays() {
     const year = parseInt(document.getElementById("year").value);
     const month = parseInt(document.getElementById("month").value);
 
+    if (isNaN(year) || isNaN(month) || month < 1 || month > 12) {
+        document.getElementById("result").textContent =
+            "Будь ласка, введіть коректні значення.";
+        return;
+    }
+
+    // Визначення кількості днів у місяці
     const days = new Date(year, month, 0).getDate();
 
     document.getElementById("result").textContent =
-        `У ${month} місяці ${year} року: ${days} днів`;
+        `У ${month}-му місяці ${year} року ${days} днів.`;
 }
